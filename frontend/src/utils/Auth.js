a3c445dddb572b07e79f07cc02b877fec01d5aa5
Marina Natalina <kiska21mk@yandex.ru>
2022-07-08 12:13:13 +0300

    ПР 15

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,8 +1,11 @@
-export const BASE_URL = "https://api.marina.nomoredomains.sbs";
+export const BASE_URL = "https://marina.nomoredomains.sbs";
 
 function checkResponse(promise) {
   return promise.then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+    if(res.ok || res.status(201)) {
+      return res.json();
+     }
+     return Promise.reject(`Ошибка: ${res.status}`);
   });
 }
 
@@ -23,6 +26,7 @@ export const register = (email, password) => {
 export const authorize = (email, password) => {
   const promise = fetch(`${BASE_URL}/signin`, {
     method: "POST",
+    credentials: 'include',
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
@@ -39,6 +43,7 @@ export const checkToken = (token) => {
   console.log("in auth-checkToken", token);
   const promise = fetch(`${BASE_URL}/users/me`, {
     method: "GET",
+    credentials: 'include',
     headers: {
       authorization: `Bearer ${token}`,
     },
