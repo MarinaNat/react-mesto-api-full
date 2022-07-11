b7284e7090d376b451e4b9a51d88e148d42f8579
Marina Natalina <kiska21mk@yandex.ru>
2022-07-11 20:42:04 +0300

    15ПР вход

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ component: Component, ...props }) => {
+  console.log(props.loggedIn) 
   return props.loggedIn ? (
     <Component {...props} />
   ) : (
