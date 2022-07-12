98bc381c376035204d85e67178de16b29ad23f8f
Marina Natalina <kiska21mk@yandex.ru>
2022-07-12 13:44:25 +0300

    15ПР

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -5,6 +5,13 @@ export default class Api {
 
   }
 
+  requestResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Все сломалось:( ${res.status}`);
+  }
+
   getHeaders() {
     const token = localStorage.getItem('jwt');
     return {
@@ -13,85 +20,85 @@ export default class Api {
     };
   }
 
-  makeRequest(res) {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Ошибка: ${res.status}`);
-  }
-
-  //данные с сервера о профиле
-  getUserInfo() {
-    return fetch(`${this.baseUrl}/users/me`, {
-      method: "GET",
-      headers: this.getHeaders(),
-    }).then(this.makeRequest);
-  }
-
   //данные с сервера о карточках
   getCard() {
     return fetch(`${this.baseUrl}/cards`, {
-      method: "GET",
+      method: 'GET',
       headers: this.getHeaders(),
-    }).then(this.makeRequest);
-  }
-
-  //отправка данных профиля
-  setUserInfo(name, about) {
-    return fetch(`${this.baseUrl}/users/me`, {
-      method: "PATCH",
-      headers: this.getHeaders(),
-      body: JSON.stringify({
-        name: name,
-        about: about,
-      }),
-    }).then(this.makeRequest);
+    })
+      .then(this.requestResponse)
   }
 
   //отправка данных карты
   addCard(name, link) {
     return fetch(`${this.baseUrl}/cards`, {
-      method: "POST",
+      method: 'POST',
       headers: this.getHeaders(),
       body: JSON.stringify({
         name,
         link
       })
-    }).then(this.makeRequest);
+    })
+      .then(this.requestResponse)
   }
 
-  //удаление карточек
-  deleteCard(id) {
-    return fetch(`${this.baseUrl}/cards/${id}`, {
-      method: "DELETE",
+  //данные с сервера о профиле
+  getUserInfo() {
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: 'GET',
       headers: this.getHeaders(),
-    }).then(this.makeRequest);
+    })
+      .then(this.requestResponse);
   }
 
-  //добавленеи, удаление лайков
-  changeLikeCardStatus(id, isLiked) {
-    return fetch(`${this.baseUrl}/cards/${id}/likes`, {
-      method: `${isLiked ? "PUT" : "DELETE"}`,
+  //отправка данных профиля
+  setUserInfo(name, job) {
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: 'PATCH',
       headers: this.getHeaders(),
-    }).then(this.makeRequest);
+      body: JSON.stringify({
+        name: name,
+        about: job
+      })
+    })
+      .then(this.requestResponse)
   }
 
   //отправка данных аватарки
   setUserAvatar(data) {
     return fetch(`${this.baseUrl}/users/me/avatar`, {
-      method: "PATCH",
+      method: 'PATCH',
       headers: this.getHeaders(),
       body: JSON.stringify({
-        avatar: data.avatar,
-      }),
-    }).then(this.makeRequest);
+        avatar: data.avatar
+      })
+    })
+      .then(this.requestResponse)
+  }
+
+  //удаление карточек
+  deleteCard(id) { 
+    return fetch(`${this.baseUrl}/cards/${id}`, {
+        method: 'DELETE',
+        headers: this.getHeaders(),
+    })
+        .then(this.requestResponse)
+}
+
+  //добавленеи, удаление лайков
+  changeLikeCardStatus(id, isLiked) {
+    return fetch(`${this.baseUrl}/cards/${id}/likes`, {
+      method: `${isLiked ? 'PUT' : 'DELETE'}`,
+        headers: this.getHeaders(),
+    })
+        .then(this.requestResponse);
   }
 }
 
-export const api = new Api({
-  baseUrl: "https://api.marina.nomoredomains.sbs",
-  headers: {
-    // authorization: "143aa2a9-cefa-4929-a9d6-e76e666a89c9",
-    "Content-Type": "application/json",
-  },
-});
+  export const api = new Api({
+    baseUrl: "https://api.marina.nomoredomains.sbs",
+    headers: {
+      // authorization: "143aa2a9-cefa-4929-a9d6-e76e666a89c9",
+      "Content-Type": "application/json",
+    },
+  });
